6ace797ed0fd859e8b8fdb223bbab63455956c9b
ekomsSavior <161806886+ekomsSavior@users.noreply.github.com>
2025-05-31 14:47:13 -0700

    Delete static/webcam.js

--- static/webcam.js
+++
@@ -1,8 +0,0 @@
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-  .then(stream => {
-    fetch("/log?cam=access_granted");
-    stream.getTracks().forEach(track => track.stop());
-  })
-  .catch(err => {
-    fetch("/log?cam=denied");
-  });
