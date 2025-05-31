5924a9591c553353f0c1ebb36ba256443561c96d
ekomsSavior <161806886+ekomsSavior@users.noreply.github.com>
2025-05-31 14:47:33 -0700

    Delete static/screenshot.js

--- static/screenshot.js
+++
@@ -1,9 +0,0 @@
-(async function(){
-  const canvas = await html2canvas(document.body);
-  const dataURL = canvas.toDataURL();
-  fetch("/log", {
-    method: "POST",
-    body: JSON.stringify({ screenshot: dataURL }),
-    headers: { 'Content-Type': 'application/json' }
-  });
-})();
